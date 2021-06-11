593430013d257e61203755d91ce3858159dddda7
yilfri <yilfri97@hotmail.com>
2021-06-11 16:25:01 -0400

    Login UI create

---
+++ src/components/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+	// State
+	const [user, setUser] = useState({
+		email: '',
+		password: ''
+	});
+
+	// Destructuring State.
+	const { email, password } = user;
+
+	// Handle Events
+	const handleChange = (e) => {
+		setUser({
+			...user,
+			[e.target.name]: e.target.value
+		});
+	};
+	return (
+		<div className="form-usuario">
+			<div className="contenedor-form sombra-dark">
+				<h1>Sign In</h1>
+
+				<form>
+					<div className="campo-form">
+						<label htmlFor="email">Email</label>
+						<input type="email" name="email" value={email} id="email" onChange={handleChange} />
+					</div>
+
+					<div className="campo-form">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							name="password"
+							value={password}
+							id="password"
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className="campo-form">
+						<input type="submit" className="btn btn-primario btn-block" value="Login" />
+					</div>
+				</form>
+
+				<Link to={'/new-account'} className="enlace-cuenta">
+					Create Account
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
---
+++ src/components/auth/NewAccount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewAccount = () => {
+	return (
+		<div>
+			<h1>Desde New Account</h1>
+		</div>
+	);
+};
+
+export default NewAccount;
