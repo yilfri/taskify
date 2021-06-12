2e74260b4f3835c11ff14ef126cfb398dda4cd98
yilfri <yilfri97@hotmail.com>
2021-06-12 11:48:03 -0400

    projectContext to newProject for conditional form render

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import projectContext from '../../context/projects/projectContext';
 
 const NewProject = () => {
+	// Getting form state.
+	const projectsContext = useContext(projectContext);
+
+	const { form } = projectsContext;
+
 	// Create state
 	const [project, setProject] = useState({
 		name: ''
@@ -25,18 +31,21 @@ const NewProject = () => {
 			<button type="button" className="btn btn-block btn-primario">
 				New Project
 			</button>
-			<form className="formulario-nuevo-proyecto" onSubmit={handleSubmit}>
-				<input
-					type="text"
-					name="name"
-					className="input-text"
-					value={name}
-					placeholder="Project Name"
-					onChange={handleChange}
-				/>
 
-				<input type="submit" value="Add Project" className="btn btn-primario btn-block" />
-			</form>
+			{form ? (
+				<form className="formulario-nuevo-proyecto" onSubmit={handleSubmit}>
+					<input
+						type="text"
+						name="name"
+						className="input-text"
+						value={name}
+						placeholder="Project Name"
+						onChange={handleChange}
+					/>
+
+					<input type="submit" value="Add Project" className="btn btn-primario btn-block" />
+				</form>
+			) : null}
 		</>
 	);
 };
