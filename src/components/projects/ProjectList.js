3cfe4b60ed526c04a50f730f86c7a8b94cf7baad
yilfri <yilfri97@hotmail.com>
2021-06-15 12:03:58 -0400

    adding transitions to projects

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react';
 import Project from './Project';
 import projectContext from '../../context/projects/projectContext';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const ProjectList = () => {
 	// Extract project from Context
@@ -17,9 +18,13 @@ const ProjectList = () => {
 
 	return (
 		<ul className="listado-proyectos">
-			{projects.map((project) => (
-				<Project key={project.id} project={project} />
-			))}
+			<TransitionGroup>
+				{projects.map((project) => (
+					<CSSTransition key={project.id} timeout={200} classNames="proyecto">
+						<Project project={project} />
+					</CSSTransition>
+				))}
+			</TransitionGroup>
 		</ul>
 	);
 };
