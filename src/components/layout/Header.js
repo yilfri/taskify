501e7ea1a6d2e509cbc7eb99e31b340f21738e37
yilfri <yilfri97@hotmail.com>
2021-06-20 18:37:15 -0400

    Showing name of authenticated user

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
 
 const Header = () => {
+	//Extract auth info.
+	const authContext = useContext(AuthContext);
+	const { user, authenticatedUser } = authContext;
+
+	useEffect(() => {
+		authenticatedUser();
+	}, []);
+
 	return (
 		<div className="app-header">
-			<p className="nombre-usuario">
-				Hola <span>José Lugo</span>
-			</p>
+			{user ? (
+				<p className="nombre-usuario">
+					Hola <span>{user.name}</span>
+				</p>
+			) : null}
 
 			<nav className="nav-principal">
 				<a href="#!">Log Out</a>
